fb6ffb5bb70854d27f0220cf05d8ec742b365990
Randomnicode <randomnicode@gmail.com>
2020-09-09 21:22:24 -0700

    Add symlinks inside container so old dbs don't have an issue during migration

--- install/docker/run.sh
+++ install/docker/run.sh
@@ -5,9 +5,19 @@ set -e
 echo "Process will run as:"
 echo "User: $(id -u)"
 echo "Group: $(id -g)"
+
+# transcode folder
 mkdir -p $AIRSONIC_DIR/airsonic/transcode
 [[ ! -f $AIRSONIC_DIR/airsonic/transcode/ffmpeg ]] && ln -fs /usr/bin/ffmpeg $AIRSONIC_DIR/airsonic/transcode/ffmpeg
 [[ ! -f $AIRSONIC_DIR/airsonic/transcode/lame ]] && ln -fs /usr/bin/lame $AIRSONIC_DIR/airsonic/transcode/lame
+
+# symlinks to facilitate migration from old airsonic docker images (dbs will contain reference to /airsonic)
+mkdir -p /airsonic
+ln -fs $AIRSONIC_DIR/airsonic /airsonic/data
+ln -fs $AIRSONIC_DIR/music /airsonic/music
+ln -fs $AIRSONIC_DIR/podcasts /airsonic/podcasts
+ln -fs $AIRSONIC_DIR/playlists /airsonic/playlists
+
 java --version
 echo "JAVA_OPTS=$JAVA_OPTS"
 $AIRSONIC_DIR/airsonic/transcode/ffmpeg -version
@@ -15,7 +25,6 @@ curl --version
 echo "PATH=$PATH"
 
 if [[ $# -lt 1 ]] || [[ ! "$1" == "java"* ]]; then
-
     java_opts_array=()
     while IFS= read -r -d '' item; do
         java_opts_array+=( "$item" )
